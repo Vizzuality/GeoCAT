c9225c1a42f93b13641521257a94a40ab718db31
Javier Álvarez Medina <jmedina@Javier-Alvarez-Medinas-MacBook-Pro.local>
2010-06-16 14:18:04 +0200

    flickr controller

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -1,16 +1,20 @@
 class FlickrController < ApplicationController
   def search
     
-    if params['q'] != NIL
-      search_text = params['q']
-      FlickRaw.api_key="ba21020f518d59d9d74b81af71b37e7c"
-      FlickRaw.shared_secret="35b1704d1c3e1e6d"
-      frob = flickr.auth.getFrob
-      @list = flickr.photos.getRecent
-      render :json =>@list
+    if params.empty?
+      render :json => "{'Status':'Error'}"
     else
-      puts "jamon"
-    end 
+      if !params['q'].empty? or !params['q'].nil?
+        q = params['q']
+        FlickRaw.api_key="ba21020f518d59d9d74b81af71b37e7c"
+        FlickRaw.shared_secret="35b1704d1c3e1e6d"
+        frob = flickr.auth.getFrob
+        @list = flickr.photos.search(:tags=>q,:extras=>'geo,tags', :has_geo=>'1')
+        render :json =>@list
+      else
+        render :json => "{'Status':'Error'}"
+      end
+    end
     
     
 
