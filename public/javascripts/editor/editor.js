fbfdf0e958c86c0b02e12848660574faafdbe36f
Javier Álvarez Medina <jmedina@Javier-Alvarez-Medinas-MacBook-Pro.local>
2010-06-21 19:27:37 +0200

    last changes

--- public/javascripts/editor/editor.js
+++ public/javascripts/editor/editor.js
@@ -2,17 +2,19 @@ var specie;  /* specie name */
 
 var map;
 var bounds;
+var flickr_founded = [];
+var gbif_founded = [];
 var flickr_data = [];
 var gbif_data = [];
 
 
-// var tooltip;
-// var overlay;
-// var global_id=1;
-// var flickr_markers = [];
-// var flickr_data;
+ var tooltip;
+ var overlay;
+ var global_id=1;
+ var flickr_markers = [];
 
 	$(document).ready(function() {
+		
 		//Get scientific_name
 		specie = $('a#scientific_name').text();
 		
@@ -28,7 +30,17 @@ var gbif_data = [];
 	
 	  map = new google.maps.Map(document.getElementById("map"), myOptions);
 		bounds = new google.maps.LatLngBounds();
-				
+		
+		
+		//open add sources container
+		$("#add_source_button").click(function(ev){
+			if (!$('#add_source_container').is(':visible')) {
+				resetSourcesProperties();
+				$("#add_source_container").fadeIn();
+				$(this).addClass('open');
+			}
+		});
+		
 		//add source effects
 		$("#add_source_container ul li a.checkbox").click(function(ev){
 			if (!$(this).parent().hasClass('selected') && !$(this).parent().hasClass('added')) {
@@ -39,6 +51,24 @@ var gbif_data = [];
 				}
 			}
 		});
+		
+		//import data
+		$("a.import_data").click(function(ev){
+			if ($(this).hasClass('enabled')) {
+				showMamufasMap();
+				$(this).text('close');
+				switch($(this).parent().parent().find('a.checkbox').attr('id')) {
+					case 'add_flickr': 	flickr_data = flickr_founded;
+															addSourceToMap(flickr_data);
+														 	break;
+					case 'add_gbif':  	gbif_data = gbif_founded;
+															addSourceToMap(gbif_data);
+															break;
+					default: 						null;
+				}
+			}
+		});
+		
 
 		//searchFlickr();
 	});
@@ -68,9 +98,9 @@ var gbif_data = [];
 				function(data){
 					console.log(data);
 					switch(kind) {
-						case 'add_flickr': 	flickr_data = data;
+						case 'add_flickr': 	flickr_founded = data;
 															 	break;
-						case 'add_gbif':  	gbif_data = data;
+						case 'add_gbif':  	gbif_founded = data;
 																break;
 						default: 						null;
 					}
@@ -103,6 +133,31 @@ var gbif_data = [];
 			$('#mamufas_map').hide('fast');
 		});	
 	}
+	
+	
+	function resetSourcesProperties() {
+		flickr_founded = [];
+		gbif_founded = [];
+		
+		$("#add_source_container ul li").each(function(i,item){
+			$(this).removeClass('selected');
+			$(this).removeClass('added');
+			$(this).find('span p').removeClass('loaded');
+			$(this).find('span a').removeClass('enabled');
+			$(this).find('span a').text('import');
+		});
+		
+		if (flickr_data.length!=0) {
+			$('#add_flickr').parent().addClass('added');
+		}
+		
+		if (gbif_data.length!=0) {
+			$('add_gbif').parent().addClass('added');
+		}
+		
+	}
+	
+	
 
 	// function toggleMarker(element) {
 	// 		if ($(element).val()==0) {
@@ -181,70 +236,68 @@ var gbif_data = [];
 	// 	
 	// 	
 	// 	
-	// 	function addSourceToMap() {
-	// 			resetProperties();
-	// 			var image = new google.maps.MarkerImage('images/editor/Flickr_marker.png',new google.maps.Size(25, 25),new google.maps.Point(0,0),new google.maps.Point(12, 12));
-	// 
-	// 				bounds = new google.maps.LatLngBounds();
-	// 				
-	// 		    $.each(flickr_data, function(i,item){
-	// 					bounds.extend(new google.maps.LatLng(item.latitude,item.longitude));
-	// 					// $('ul').append('<li><input type="checkbox" checked="true" value="'+global_id+'"/><label>'+item.title+'</label></li>');
-	// 					var object = new Object();
-	// 					object.global_id = global_id;
-	// 					object.item = item;
-	// 					var marker = new google.maps.Marker({
-	// 					        position: new google.maps.LatLng(item.latitude,item.longitude), 
-	// 									draggable: true,
-	// 					        map: map,
-	// 					        title: item.title,
-	// 									icon: image,
-	// 									data:object
-	// 					    });
-	// 					
-	// 					google.maps.event.addListener(marker,"click",function(ev){
-	// 						if (overlay!=null) {
-	// 							overlay.changePosition(new google.maps.LatLng(this.position.b,this.position.c),this.data.global_id,this.data.item);
-	// 							overlay.show();
-	// 						} else {
-	// 							overlay = new MarkerTooltip(new google.maps.LatLng(this.position.b,this.position.c), this.data.global_id, this.data.item,map);
-	// 						}
-	// 					});
-	// 					
-	// 					google.maps.event.addListener(marker,"dragstart",function(){
-	// 						if (overlay!=null) {
-	// 							overlay.hide();
-	// 						}
-	// 					});
-	// 					
-	// 
-	// 			    var circle = new google.maps.Circle({
-	//           	map: map,
-	//           	radius: item.accuracy*2000,
-	//  						strokeColor: "pink",
-	// 						strokeOpacity: 0.5,
-	// 						strokeWeight: 1,
-	// 						fillOpacity: 0.5,
-	// 						fillColor:"pink"
-	//         	});
-	// 
-	// 					circle.bindTo('map', marker);
-	//         	circle.bindTo('center', marker, 'position');
-	// 					global_id++;
-	// 					flickr_markers.push(marker);					
-	//  		    });
-	// 				map.fitBounds(bounds);
-	// 
-	// 				//MAP EVENTS
-	// 				google.maps.event.addListener(map,"bounds_changed",function(){
-	// 					if (overlay!=null) {
-	// 						overlay.hide();
-	// 					}
-	// 				});
-	// 
-	// 				$('ul li input[type="checkbox"]').change(function(ev){toggleMarker(this)});
-	// 				
-	// 	}
+		function addSourceToMap(information) {
+				var image = new google.maps.MarkerImage('images/editor/Flickr_marker.png',new google.maps.Size(25, 25),new google.maps.Point(0,0),new google.maps.Point(12, 12));
+					console.log(information);
+					
+			    $.each(information[0].data, function(i,item){
+						bounds.extend(new google.maps.LatLng(item.latitude,item.longitude));
+						var object = new Object();
+						object.global_id = global_id;
+						object.item = item;
+						var marker = new google.maps.Marker({
+						        position: new google.maps.LatLng(item.latitude,item.longitude), 
+										draggable: true,
+						        map: map,
+						        title: item.title,
+										icon: image,
+										data:object
+						    });
+						
+						google.maps.event.addListener(marker,"click",function(ev){
+							if (overlay!=null) {
+								overlay.changePosition(new google.maps.LatLng(this.position.b,this.position.c),this.data.global_id,this.data.item);
+								overlay.show();
+							} else {
+								overlay = new MarkerTooltip(new google.maps.LatLng(this.position.b,this.position.c), this.data.global_id, this.data.item,map);
+							}
+						});
+						
+						google.maps.event.addListener(marker,"dragstart",function(){
+							if (overlay!=null) {
+								overlay.hide();
+							}
+						});
+						
+	
+				    var circle = new google.maps.Circle({
+	          	map: map,
+	          	radius: item.accuracy*2000,
+	 						strokeColor: "pink",
+							strokeOpacity: 0.5,
+							strokeWeight: 1,
+							fillOpacity: 0.5,
+							fillColor:"pink"
+	        	});
+	
+						circle.bindTo('map', marker);
+	        	circle.bindTo('center', marker, 'position');
+						global_id++;
+						flickr_markers.push(marker);					
+	 		    });
+					map.fitBounds(bounds);
+	
+					//MAP EVENTS
+					google.maps.event.addListener(map,"bounds_changed",function(){
+						if (overlay!=null) {
+							overlay.hide();
+						}
+					});
+					
+					hideMamufasMap();
+	
+					
+		}
 	
 	
 	
