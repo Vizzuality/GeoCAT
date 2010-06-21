d2eddcde8395588dfb581848b7ddd03017a082a5
Raúl Barroso <ra.barroso@gmail.com>
2010-06-21 18:20:01 +0200

    Flickr service changed

--- app/models/rla.rb
+++ app/models/rla.rb
@@ -2,17 +2,18 @@ class Rla < ActiveRecord::Base
   belongs_to :user
   
   validates_presence_of :name, :data  
-  
-  
-  
-  
-  def self.save(upload)
-      name =  upload['datafile'].original_filename
+    
+  def self.save
+      
+
+      name =  rlas['datafile'].original_filename
       directory = "public/data"
       # create the file path
       path = File.join(directory, name)
       # write the file
-      File.open(path, "wb") { |f| f.write(upload['datafile'].read) }
+      File.open(path, "wb") { |f| f.write(rlas['datafile'].read) }
+
+
   end
   
   
