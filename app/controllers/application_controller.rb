24c1ae37ee4a40254b936cca855b444da0cdcf45
Fernando Espinosa <fer@ferdev.com>
2010-11-12 13:46:13 +0100

    Adds filter to check if browser is html5 compliant.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+  class NoHTML5Compliant < Exception; end;
+
   rescue_from Exception, :with => :render_500
+  rescue_from NoHTML5Compliant, :with => :no_html5_compliant
+
+  before_filter :browser_is_html5_compliant?
 
   #protect_from_forgery
   layout 'application'
@@ -19,4 +24,16 @@ class ApplicationController < ActionController::Base
 
     render :file => "#{Rails.root}/public/500.html", :status => 500, :layout => false
   end
+
+  def no_html5_compliant
+    render :file => "#{Rails.root}/public/HTML5.html", :status => 500, :layout => false
+  end
+
+  private
+    def browser_is_html5_compliant?
+      user_agent = request.user_agent.downcase
+      unless user_agent.blank? || user_agent.match(/firefox\/3\.6|safari\/5|chrome\/7|opera\/10/)
+        raise NoHTML5Compliant
+      end
+    end
 end
