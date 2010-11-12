e3e0697e019da9677981177b1eff94b9e075175d
Fernando Espinosa <fer@ferdev.com>
2010-11-12 11:47:13 +0100

    Adds route to catch routing errors and redirects the request to 404.html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery
   layout 'application'
+
+  def render_404(exception = nil)
+    if exception
+        logger.info "Rendering 404: #{exception.message}"
+    end
+
+    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+  end
 end
