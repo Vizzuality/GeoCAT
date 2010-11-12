afe69bd5863c10308e2967072b0843977b5b7cd7
Fernando Espinosa <fer@ferdev.com>
2010-11-12 14:05:08 +0100

    Adds backtrace output in rescue from exceptions action, and removes params validation in FileController#upload action.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
   def render_404(exception = nil)
     if exception
       logger.info "Rendering 404: #{exception.message}"
+      logger.info "Backtrace: #{exception.backtrace}"
     end
 
     render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
@@ -19,7 +20,9 @@ class ApplicationController < ActionController::Base
 
   def render_500(exception = nil)
     if exception
-     logger.info "Rendering 500: #{exception.message}"
+      logger.info "Rendering 500: #{exception.message}"
+      logger.info 'Backtrace:'
+      logger.info exception.backtrace.join("\n")
     end
 
     render :file => "#{Rails.root}/public/500.html", :status => 500, :layout => false
