ba9b85b44684483244c1e6c22748c951225d0619
Fernando Espinosa <fer@ferdev.com>
2010-11-12 12:13:39 +0100

    Adds rescue from generic exceptions and redirects the requests to 500.html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
 class ApplicationController < ActionController::Base
+  rescue_from Exception, :with => :render_500
+
   #protect_from_forgery
   layout 'application'
 
   def render_404(exception = nil)
     if exception
-        logger.info "Rendering 404: #{exception.message}"
+      logger.info "Rendering 404: #{exception.message}"
     end
 
     render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+  end
+
+  def render_500(exception = nil)
+    if exception
+     logger.info "Rendering 500: #{exception.message}"
+    end
+
+    render :file => "#{Rails.root}/public/500.html", :status => 500, :layout => false
   end
 end
