59533cfc6bb87c8cbe2a1ee2db16f49b4bda1009
jatorre <jatorre@vizzuality.com>
2010-11-23 17:50:37 +0100

    make use of multiple gbif mirrors for queries

--- app/controllers/gbif_controller.rb
+++ app/controllers/gbif_controller.rb
@@ -2,86 +2,86 @@ class GbifController < ApplicationController
 
     # begin SEARCH
     def search
-      if params.empty?
-        render :json => "{'Status':'Error'}"
-      else
-        if !params[:q].empty? or !params[:q].nil?
-          q = params[:q]
+      
+      render :json => "{'Status':'Error'}" and return if params.empty?
+      render :json => "{'Status':'Error'}" and return if params[:q].blank?
 
-          # TO GET THE POINTS BY SPECIE -
-          # Puma in this case (13191720)
-          # Lepidoptera (13141205)
-
-          # q = "13191720"
-          require 'open-uri'
-
+      q = params[:q]
+      
+      require 'open-uri'
+      
+      gbif_mirrors = %w(data.gbif.org es.mirror.gbif.org de.mirror.gbif.org)
+      
+      gbif_mirrors.each do |mirror|
+        begin
           gbif_url = URI.escape(
-            "http://de.mirror.gbif.org/ws" +
+            "http://#{mirror}/ws" +
             "/rest/Occurrence/list?georeferencedonly=true&format=darwin" +
             "&maxresults=200&coordinateissues=false&scientificname=#{q}"
           )
+          puts "trying #{gbif_url}"
           open(gbif_url) {|f| @list =  f.read }
+          break
+        rescue Exception=>e
+        end
+      end
+      
+      doc = Nokogiri::XML(@list)
+      
+      points = []
 
-          doc = Nokogiri::XML(@list)
-          
-          points = []
-
-          doc.xpath("//gbif:occurrenceRecords/to:TaxonOccurrence").each do |node|
-                        
-            @institutionCode          = node.xpath("to:institutionCode").text
-            @collectionCode           = node.xpath("to:collectionCode").text
-            @catalogNumber            = node.xpath("to:catalogNumber").text
-            @basisOfRecord            = node.xpath("to:basisOfRecordString").text
-            @recordedBy               = node.xpath("to:collector").text
-            @eventDate                = node.xpath("to:earliestDateCollected").text
-            @country                  = node.xpath("to:country").text
-            @stateProvince            = node.xpath("to:stateProvince").text
-            @county                   = node.xpath("to:county").text
-            @verbatimElevation        = ""
-            @locality                 = node.xpath("to:locality").text
-            @coordinateUncertaintyInMeters = node.xpath("to:coordinateUncertaintyInMeters").text
-            @identifiedBy             = ""
-            @occurrenceRemarks        = node.xpath("to:gbifNotes").text
-            @occurrenceDetails        = ""
-            
-            
-            @gbifKey                  = node.xpath("to:gbifNotes").attr('gbifKey')
-            @latitude                 = node.xpath("to:decimalLatitude").text.to_f
-            @longitude                = node.xpath("to:decimalLongitude").text.to_f
-            
-            if (@institutionCode =="LSUMZ" and @collectionCode=="Mammals" and @catalogNumber=="4115")
-              points << {"latitude"=> @latitude,"longitude"=> @longitude,
-                "institutionCode"=>@institutionCode,
-                "collectionCode"=>@collectionCode,
-                "catalogNumber"=>@catalogNumber,
-                "basisOfRecord"=>@basisOfRecord,
-                "recordedBy"=>@recordedBy,
-                "eventDate" =>@eventDate,
-                "country"=>@country,
-                "stateProvince"=>@stateProvince,
-                "county"=>@county,
-                "verbatimElevation"=>@verbatimElevation,
-                "locality"=>@locality,
-                "coordinateUncertaintyInMeters"=>@coordinateUncertaintyInMeters,
-                "identifiedBy"=>@identifiedBy,
-                "occurrenceRemarks"=>@occurrenceRemarks,
-                "occurrenceDetails"=>@occurrenceDetails,
-                "coordinateUncertaintyInMeters"=>"15","collector"=>@gbifKey,"active"=>true,"removed"=>false,
-                "catalogue_id"=>"gbif_#{@institutionCode}-#{@collectionCode}-#{@catalogNumber}",
-                "kind"=>"gbif",
-                "description" => ""}
-            end
-          end
-
-          @list =  [{"id"=>"gbif_id","name"=>"gbif","points"=> points }]
-
-          render :json =>@list
-
-        else
-          render :json => "{'Status':'Error'}"
+      doc.xpath("//gbif:occurrenceRecords/to:TaxonOccurrence").each do |node|
+                    
+        @institutionCode          = node.xpath("to:institutionCode").text
+        @collectionCode           = node.xpath("to:collectionCode").text
+        @catalogNumber            = node.xpath("to:catalogNumber").text
+        @basisOfRecord            = node.xpath("to:basisOfRecordString").text
+        @recordedBy               = node.xpath("to:collector").text
+        @eventDate                = node.xpath("to:earliestDateCollected").text
+        @country                  = node.xpath("to:country").text
+        @stateProvince            = node.xpath("to:stateProvince").text
+        @county                   = node.xpath("to:county").text
+        @verbatimElevation        = ""
+        @locality                 = node.xpath("to:locality").text
+        @coordinateUncertaintyInMeters = node.xpath("to:coordinateUncertaintyInMeters").text
+        @identifiedBy             = ""
+        @occurrenceRemarks        = node.xpath("to:gbifNotes").text
+        @occurrenceDetails        = ""
+        
+        
+        @gbifKey                  = node.xpath("to:gbifNotes").attr('gbifKey')
+        @latitude                 = node.xpath("to:decimalLatitude").text.to_f
+        @longitude                = node.xpath("to:decimalLongitude").text.to_f
+        
+        if (@institutionCode =="LSUMZ" and @collectionCode=="Mammals" and @catalogNumber=="4115")
+          points << {"latitude"=> @latitude,"longitude"=> @longitude,
+            "institutionCode"=>@institutionCode,
+            "collectionCode"=>@collectionCode,
+            "catalogNumber"=>@catalogNumber,
+            "basisOfRecord"=>@basisOfRecord,
+            "recordedBy"=>@recordedBy,
+            "eventDate" =>@eventDate,
+            "country"=>@country,
+            "stateProvince"=>@stateProvince,
+            "county"=>@county,
+            "verbatimElevation"=>@verbatimElevation,
+            "locality"=>@locality,
+            "coordinateUncertaintyInMeters"=>@coordinateUncertaintyInMeters,
+            "identifiedBy"=>@identifiedBy,
+            "occurrenceRemarks"=>@occurrenceRemarks,
+            "occurrenceDetails"=>@occurrenceDetails,
+            "coordinateUncertaintyInMeters"=>"15","collector"=>@gbifKey,"active"=>true,"removed"=>false,
+            "catalogue_id"=>"gbif_#{@institutionCode}-#{@collectionCode}-#{@catalogNumber}",
+            "kind"=>"gbif",
+            "description" => ""}
         end
       end
 
+      @list =  [{"id"=>"gbif_id","name"=>"gbif","points"=> points }]
+
+      render :json =>@list
+    rescue Exception=> e
+      render :json => "{'Status':'Error',message:'#{e.message}'}"
     end
     # end SEARCH
 end
