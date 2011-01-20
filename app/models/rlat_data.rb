e6efaf3e4b742c50531fa4c4ef79b2dd872655ed
Fernando Espinosa <fer@ferdev.com>
2011-01-20 16:53:08 +0100

    Replace name field's name to 'recordSource'

--- app/models/rlat_data.rb
+++ app/models/rlat_data.rb
@@ -48,7 +48,7 @@ class RlatData
     sources.each do |source|
       data += source['points'].collect do |point|
         {
-          'name'                          => source['name'],
+          'recordSource'                  => source['recordSource'],
           'scientificname'                => scientificname,
           'latitude'                      => point['latitude'],
           'longitude'                     => point['longitude'],
@@ -122,26 +122,26 @@ class RlatData
       }]
       csv.each do |row|
         self.sources.first['points'].push({
-          'kind'                          => row.respond_to?(:name)                                       ? row.name                                       : 'your',
-          'latitude'                      => row.respond_to?(:latitude)                                   ? row.latitude                                   : nil,
-          'longitude'                     => row.respond_to?(:longitude)                                  ? row.longitude                                  : nil,
-          'collector'                     => row.respond_to?(:collector)                                  ? row.collectioncode                             : nil,
-          'coordinateUncertaintyInMeters' => row.respond_to?(:coordinateuncertaintyinmeters)              ? row.coordinateuncertaintyinmeters              : nil,
-          'catalogue_id'                  => row.respond_to?(:catalogue_id)                               ? row.catalogue_id                               : nil,
-          'collectionCode'                => row.respond_to?(:collectioncode)                             ? row.collectioncode                             : nil,
-          'institutionCode'               => row.respond_to?(:institutioncode)                            ? row.institutioncode                            : nil,
-          'catalogNumber'                 => row.respond_to?(:catalognumber)                              ? row.catalognumber                              : nil,
-          'basisOfRecord'                 => row.respond_to?(:basisofrecord)                              ? row.basisofrecord                              : nil,
-          'eventDate'                     => row.respond_to?(:eventdate)                                  ? row.eventdate                                  : nil,
-          'country'                       => row.respond_to?(:country)                                    ? row.country                                    : nil,
-          'stateProvince'                 => row.respond_to?(:stateprovince)                              ? row.stateprovince                              : nil,
-          'county'                        => row.respond_to?(:county)                                     ? row.county                                     : nil,
-          'verbatimElevation'             => row.respond_to?(:verbatimelevation)                          ? row.verbatimelevation                          : nil,
-          'locality'                      => row.respond_to?(:locality)                                   ? row.locality                                   : nil,
-          'coordinateUncertaintyText'     => row.respond_to?(:coordinateuncertaintytext)                  ? row.coordinateuncertaintytext                  : nil,
-          'identifiedBy'                  => row.respond_to?(:identifiedby)                               ? row.identifiedby                               : nil,
-          'occurrenceRemarks'             => row.respond_to?(:occurrenceremarks)                          ? row.occurrenceremarks                          : nil,
-          'occurrenceDetails'             => row.respond_to?(:occurrencedetails)                          ? row.occurrencedetails                          : nil
+          'recordSource'                  => row.respond_to?(:recordsource)                                       ? row.recordsource                                       : 'Added by user',
+          'latitude'                      => row.respond_to?(:latitude)                                           ? row.latitude                                           : nil,
+          'longitude'                     => row.respond_to?(:longitude)                                          ? row.longitude                                          : nil,
+          'collector'                     => row.respond_to?(:collector)                                          ? row.collectioncode                                     : nil,
+          'coordinateUncertaintyInMeters' => row.respond_to?(:coordinateuncertaintyinmeters)                      ? row.coordinateuncertaintyinmeters                      : nil,
+          'catalogue_id'                  => row.respond_to?(:catalogue_id)                                       ? row.catalogue_id                                       : nil,
+          'collectionCode'                => row.respond_to?(:collectioncode)                                     ? row.collectioncode                                     : nil,
+          'institutionCode'               => row.respond_to?(:institutioncode)                                    ? row.institutioncode                                    : nil,
+          'catalogNumber'                 => row.respond_to?(:catalognumber)                                      ? row.catalognumber                                      : nil,
+          'basisOfRecord'                 => row.respond_to?(:basisofrecord)                                      ? row.basisofrecord                                      : nil,
+          'eventDate'                     => row.respond_to?(:eventdate)                                          ? row.eventdate                                          : nil,
+          'country'                       => row.respond_to?(:country)                                            ? row.country                                            : nil,
+          'stateProvince'                 => row.respond_to?(:stateprovince)                                      ? row.stateprovince                                      : nil,
+          'county'                        => row.respond_to?(:county)                                             ? row.county                                             : nil,
+          'verbatimElevation'             => row.respond_to?(:verbatimelevation)                                  ? row.verbatimelevation                                  : nil,
+          'locality'                      => row.respond_to?(:locality)                                           ? row.locality                                           : nil,
+          'coordinateUncertaintyText'     => row.respond_to?(:coordinateuncertaintytext)                          ? row.coordinateuncertaintytext                          : nil,
+          'identifiedBy'                  => row.respond_to?(:identifiedby)                                       ? row.identifiedby                                       : nil,
+          'occurrenceRemarks'             => row.respond_to?(:occurrenceremarks)                                  ? row.occurrenceremarks                                  : nil,
+          'occurrenceDetails'             => row.respond_to?(:occurrencedetails)                                  ? row.occurrencedetails                                  : nil
         })
       end
     end
@@ -152,7 +152,7 @@ class RlatData
       sources_warnings = []
       if self.sources.present?
         self.sources.each do |source|
-          sources_errors << 'you must provide a source name' if source['name'].blank?
+          sources_errors << 'you must provide a source name' if source['recordSource'].blank?
           source['points'].each do |point|
             if point['latitude'].blank? || point['longitude'].blank?
                invalid_points.push(point)
@@ -169,7 +169,7 @@ class RlatData
           removed_dupes = Hash[source['points'].map{|x| [x['catalogue_id'], x]}].values
           if source['points'].length > removed_dupes.length
             source['points'] = removed_dupes
-            sources_warnings << "#{source['name']} source has duplicated catalogue_id's"
+            sources_warnings << "#{source['recordSource']} source has duplicated catalogue_id's"
           end
         end
       else
