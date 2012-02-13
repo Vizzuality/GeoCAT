e66b3b907703be2a860d61c4e1bb9a223f4bfdc1
Javier Alvarez <xavijam@gmail.com>
2012-02-13 17:01:23 +0100

    merged with @ferdev :)

--- public/javascripts/editor/Models/GeoCATOperations.js
+++ public/javascripts/editor/Models/GeoCATOperations.js
@@ -96,31 +96,6 @@
 		}
 
 
-		jQuery.extend({
-    stringify  : function stringify(obj) {
-        var t = typeof (obj);
-        if (t != "object" || obj === null) {
-            // simple data type
-            if (t == "string") obj = '"' + obj + '"';
-            return String(obj);
-        } else {
-            // recurse array or object
-            var n, v, json = [], arr = (obj && obj.constructor == Array);
-
-            for (n in obj) {
-                v = obj[n];
-                t = typeof(v);
-                if (obj.hasOwnProperty(n)) {
-                    if (t == "string") v = '"' + v + '"'; else if (t == "object" && v !== null) v = jQuery.stringify(v);
-                    json.push((arr ? "" : '"' + n + '":') + String(v));
-                }
-            }
-            return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
-        }
-    }
-});
-
-
 
 		/*========================================================================================================================*/
 		/* Create the object for download later as a .geocat file. */
