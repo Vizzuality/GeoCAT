6eba6ea7ee89e6ed60c930e331d56c5b201ba76e
Adrián Pérez <adrian@simbiotica.es>
2015-03-03 11:23:44 +0100

    cleaning

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -13,8 +13,6 @@ class FlickrController < ApplicationController
       if !params[:q].empty? or !params[:q].nil?
         @name_specie = params[:q]
         response = Typhoeus.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=536b7dd52fb0348e3cb6e94d02f94b95&text=#{@name_specie}+&has_geo=1&per_page=5000&format=json&nojsoncallback=1", headers: { "Accept" => "application/json" })
-
-         puts JSON.parse(response.body)
          # end to filter
 
         render :json =>JSON.parse(response.body)
