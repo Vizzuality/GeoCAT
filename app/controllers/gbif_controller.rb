88e3bfe1578acd9cf401423fa80edae2fd22cb04
Adrián Pérez <adrian@simbiotica.es>
2015-03-03 16:27:44 +0100

    Filter 0,0 data and ask for LAST YEAR data no matter what

--- app/controllers/gbif_controller.rb
+++ app/controllers/gbif_controller.rb
@@ -10,33 +10,35 @@ class GbifController < ApplicationController
 
       require 'open-uri'
 
-      response = Typhoeus.get("http://api.gbif.org/v1/occurrence/search?isGeoreferenced=true&format=darwin&limit=300&coordinateissues=false&eventDate=2014,2015&scientificName=#{q}", headers: { "Accept" => "application/json" })
+      response = Typhoeus.get("http://api.gbif.org/v1/occurrence/search?isGeoreferenced=true&format=darwin&limit=300&coordinateissues=false&eventDate=#{Time.now.year-1}&scientificName=#{q}", headers: { "Accept" => "application/json" })
 
       points = JSON.parse(response.body)['results'].map do |item|
-        {
-          'latitude'                      => item['decimalLatitude']  || 0,
-          'longitude'                     => item['decimalLongitude'] || 0,
-          'institutionCode'               => item['institutionCode'],
-          'collectionCode'                => item['collectionCode'],
-          'catalogNumber'                 => item['catalogNumber'],
-          'scientificName'                => item['scientificName'],
-          'gbifKey'                       => item['key'],
-          'basisOfRecord'                 => item['basisOfRecord'],
-          'recordedBy'                    => item['recordedBy'],
-          'eventDate'                     => item['eventDate'],
-          'country'                       => item['country'],
-          'locality'                      => item['verbatimLocality'],
-          'coordinateUncertaintyInMeters' => item['coordinateUncertaintyInMeters'] || 15000,
-          'identifiedBy'                  => item['identifiedBy'],
-          'occurrenceRemarks'             => item['occurrenceRemarks'],
-          'collector'                     => item['recordedBy'],
-          'collectorNumber'               => item['collectorNumber'],
-          'geocat_active'                 => true,
-          'geocat_removed'                => false,
-          'geocat_kind'                   => 'gbif',
-          'geocat_alias'                  => CGI.unescape(q),
-          'geocat_query'                  => CGI.unescape(q)
-        }
+        if item['decimalLatitude'] && item['decimalLatitude'] != 0
+          {
+            'latitude'                      => item['decimalLatitude']  || 0,
+            'longitude'                     => item['decimalLongitude'] || 0,
+            'institutionCode'               => item['institutionCode'],
+            'collectionCode'                => item['collectionCode'],
+            'catalogNumber'                 => item['catalogNumber'],
+            'scientificName'                => item['scientificName'],
+            'gbifKey'                       => item['key'],
+            'basisOfRecord'                 => item['basisOfRecord'],
+            'recordedBy'                    => item['recordedBy'],
+            'eventDate'                     => item['eventDate'],
+            'country'                       => item['country'],
+            'locality'                      => item['verbatimLocality'],
+            'coordinateUncertaintyInMeters' => item['coordinateUncertaintyInMeters'] || 15000,
+            'identifiedBy'                  => item['identifiedBy'],
+            'occurrenceRemarks'             => item['occurrenceRemarks'],
+            'collector'                     => item['recordedBy'],
+            'collectorNumber'               => item['collectorNumber'],
+            'geocat_active'                 => true,
+            'geocat_removed'                => false,
+            'geocat_kind'                   => 'gbif',
+            'geocat_alias'                  => CGI.unescape(q),
+            'geocat_query'                  => CGI.unescape(q)
+          }
+        end
       end
 
       @list =  [{"specie"=>q,"name"=>"gbif","points"=> points, "zoom"=>"3" }]
