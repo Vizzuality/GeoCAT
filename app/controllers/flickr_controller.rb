b776cd69de864d34c3202214fcfb3056147ac7e9
Adrián Pérez <adrian@simbiotica.es>
2015-03-16 15:07:26 +0100

    Fix: flickr API working, up to 250 results

--- app/controllers/flickr_controller.rb
+++ app/controllers/flickr_controller.rb
@@ -11,11 +11,27 @@ class FlickrController < ApplicationController
       render :json => "{'Status':'Error'}"
     else
       if !params[:q].empty? or !params[:q].nil?
-        @name_specie = params[:q]
-        response = Typhoeus.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=536b7dd52fb0348e3cb6e94d02f94b95&text=#{@name_specie}+&has_geo=1&per_page=5000&format=json&nojsoncallback=1", headers: { "Accept" => "application/json" })
-         # end to filter
+        q = params[:q]
+        response = Typhoeus.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d5446dd5567d62b2ac43d0ec66631668&text=#{q}+&has_geo=1&per_page=5000&format=json&extras=geo&nojsoncallback=1", headers: { "Accept" => "application/json" })
 
-        render :json =>JSON.parse(response.body)
+        json_only = JSON.parse(response.body)['photos']['photo'].map do |photo|
+          {
+            'latitude'                      => photo['latitude'],
+            'longitude'                     => photo['longitude'],
+            'coordinateUncertaintyInMeters' => 15000,
+            "collector"                     => photo['owner'],
+            "geocat_active"                 => true,
+            "geocat_removed"                => false,
+            "geocat_alias"                  => CGI.unescape(q),
+            "catalogue_id"                  => "flickr_"+ q + "_" + photo['id'],
+            "geocat_kind"                   => "flickr",
+            "occurrenceRemarks"             => "#{photo['title']} / #{photo['description']}",
+            "geocat_query"                  => CGI.unescape(q)
+          }
+        end
+
+        @json_head = [{"id"=>"flickr_id","name"=>"flickr","points"=>json_only, "specie"=> q, "zoom"=>"3"}]
+        render :json =>@json_head
 
       else
         render :json => "{'Status':'Error'}"
