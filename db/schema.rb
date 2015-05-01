2190893969358fdae381f87551efb0923395fe79
Simao Belchior <simaobelchior@gmail.com>
2015-05-01 18:19:28 +0100

    Upgraded nokogiri version. removed .rvmrc as we already have ruby-version on the folder.

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -15,8 +16,8 @@ ActiveRecord::Schema.define(:version => 20100615143439) do
   create_table "geocat", :force => true do |t|
     t.string   "name"
     t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.integer  "zoom"
     t.text     "specie"
     t.text     "center"
@@ -26,8 +27,8 @@ ActiveRecord::Schema.define(:version => 20100615143439) do
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
