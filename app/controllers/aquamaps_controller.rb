4f11f773733d09caf8608b88c5bf694c378c53d3
Simao Belchior <simaobelchior@gmail.com>
2015-06-01 13:02:00 +0100

    Integrated with aquamaps via cartodb

--- app/controllers/aquamaps_controller.rb
+++ app/controllers/aquamaps_controller.rb
@@ -4,14 +4,23 @@ class AquamapsController < ApplicationController
     def search
       @points = []
 
-      render :json => "{'Status':'Error'}" and return if params.empty?
       render :json => "{'Status':'Error'}" and return if params[:q].blank?
 
       q = params[:q]
 
+      # get actual_table_name
+      require 'uri'
+      api_url = URI.encode("http://eubon-geocat.cartodb.com/api/v2/sql?q=SELECT species FROM mapping_table WHERE species iLIKE '#{q}%' LIMIT 1")
+      response = JSON.parse(Typhoeus.get(api_url).body)
+      @species_name = response['rows'] && response['rows'].first['species']
+      render :json => "{'Status':'Error'}" and return unless @species_name
+
+      table_name = "exp_#{@species_name.gsub(" ", "_")}"
       for i in 0..2
-        offset = 300 * (i * 1)
-        response = Typhoeus.get("http://api.gbif.org/v1/occurrence/search?isGeoreferenced=true&format=darwin&limit=300&coordinateissues=false&scientificName=#{q}&offset=#{offset}", headers: { "Accept" => "application/json" })
+        offset = 300 * i
+        request_uri = URI.encode("http://eubon-geocat.cartodb.com/api/v2/sql?q=SELECT * FROM #{table_name} LIMIT 300 OFFSET #{offset}")
+        response = Typhoeus.get(request_uri,
+          headers: { "Accept" => "application/json" })
         populate(response, q)
       end
 
@@ -23,33 +32,18 @@ class AquamapsController < ApplicationController
     # end SEARCH
 
     def populate(response, q)
-      JSON.parse(response.body)['results'].map do |item|
-        if item['decimalLatitude'] && item['decimalLatitude'] != 0
-          @points.push({
-            'latitude'                      => item['decimalLatitude'],
-            'longitude'                     => item['decimalLongitude'],
-            'institutionCode'               => item['institutionCode'],
-            'collectionCode'                => item['collectionCode'],
-            'catalogNumber'                 => item['catalogNumber'],
-            'scientificName'                => item['scientificName'],
-            'gbifKey'                       => item['key'],
-            'basisOfRecord'                 => item['basisOfRecord'],
-            'recordedBy'                    => item['recordedBy'],
-            'eventDate'                     => item['eventDate'],
-            'country'                       => item['country'],
-            'locality'                      => item['verbatimLocality'],
-            'coordinateUncertaintyInMeters' => item['coordinateUncertaintyInMeters'] || 15000,
-            'identifiedBy'                  => item['identifiedBy'],
-            'occurrenceRemarks'             => item['occurrenceRemarks'],
-            'collector'                     => item['recordedBy'],
-            'collectorNumber'               => item['collectorNumber'],
-            'geocat_active'                 => true,
-            'geocat_removed'                => false,
-            'geocat_kind'                   => 'aquamaps',
-            'geocat_alias'                  => CGI.unescape(q),
-            'geocat_query'                  => CGI.unescape(q)
-          })
-        end
+      JSON.parse(response.body)['rows'].map do |item|
+        @points.push({
+          'latitude'                      => item['centerlat'],
+          'longitude'                     => item['centerlong'],
+          'institutionCode'               => 'AquaMaps',
+          'scientificName'                => @species_name.titleize,
+          'geocat_active'                 => true,
+          'geocat_removed'                => false,
+          'geocat_kind'                   => 'aquamaps',
+          'geocat_alias'                  => CGI.unescape(q),
+          'geocat_query'                  => CGI.unescape(q)
+        })
       end
     end
 
