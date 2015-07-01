5511156c5f4fdae13c0d1c802f0135a15645cf0e
Adrián Pérez <adrian@simbiotica.es>
2015-07-01 13:41:28 +0200

    Add session storage variables with group symbols and check we dont step over previously created groups

--- app/assets/javascripts/editor/Layout/Groups/groups_view.js
+++ app/assets/javascripts/editor/Layout/Groups/groups_view.js
@@ -9,6 +9,7 @@
     initialize: function() {
       this._initViews();
       this._initBinds();
+      sessionStorage.clear();
     },
 
     render: function() {
@@ -34,6 +35,29 @@
     },
 
     _addGroup: function(m) {
+      var symbolsArray = ['♔','♕','♖','♗','♘','♙'];
+      var currentSymbol = null;
+      if (!sessionStorage.getItem('groupsSymbols')) {
+        var session_group = JSON.stringify(
+          {
+            g_ : {
+              'name': m.attributes.name,
+              'symbol': symbolsArray[0]
+            }
+          });
+        sessionStorage.setItem('groupsSymbols', session_group);
+        currentSymbol = symbolsArray[0];
+      } else {
+        var session_group = JSON.parse(sessionStorage.getItem('groupsSymbols'));
+        var total_arrays = Object.keys(session_group).length;
+        session_group[m.attributes.name] = {
+            'name': m.attributes.name,
+            'symbol': symbolsArray[total_arrays]
+          }
+        sessionStorage.setItem('groupsSymbols', JSON.stringify(session_group));
+        currentSymbol = symbolsArray[total_arrays];
+      }
+      m['attributes']['symbol'] = currentSymbol;
       var v = new GroupItem({ model: m });
       this.$('ul.groups').append(v.render().el);
       this.addView(v);
@@ -46,6 +70,7 @@
         if (m.get('name') === value && m.cid == cid && !m.get('removed')) {
           // Set active group
           m.set('active', true);
+          sessionStorage.setItem('currentSymbol', m.attributes.symbol);
           // Change sources_collection global variable :S
           sources_collection = m.getSources();
           analysis_view.hideAnalysis();
