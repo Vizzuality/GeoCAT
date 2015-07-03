6bc5bb50a63cc80fad294b2ab568a6c0b349ca2f
Adrián Pérez <adrian@simbiotica.es>
2015-07-03 12:25:42 +0200

    Fix: symbols per group when there is no file loaded

--- app/assets/javascripts/editor/Layout/Groups/groups_view.js
+++ app/assets/javascripts/editor/Layout/Groups/groups_view.js
@@ -35,6 +35,31 @@
     },
 
     _addGroup: function(m) {
+      if (! !!m['attributes']['symbol']) {      
+        var symbolsArray = ['♥','☓','⚊','⚬','☐'];
+        var currentSymbol = null;
+        if (!sessionStorage.getItem('groupsSymbols')) {
+          var session_group = JSON.stringify(
+            {
+              g_ : {
+                'name': m.attributes.name,
+                'symbol': symbolsArray[0]
+              }
+            });
+          sessionStorage.setItem('groupsSymbols', session_group);
+          currentSymbol = symbolsArray[0];
+        } else {
+          var session_group = JSON.parse(sessionStorage.getItem('groupsSymbols'));
+          var total_arrays = Object.keys(session_group).length;
+          session_group[m.attributes.name] = {
+              'name': m.attributes.name,
+              'symbol': symbolsArray[total_arrays]
+            }
+          sessionStorage.setItem('groupsSymbols', JSON.stringify(session_group));
+          currentSymbol = symbolsArray[total_arrays];
+        }
+        m['attributes']['symbol'] = currentSymbol;
+      }
       var v = new GroupItem({ model: m });
       this.$('ul.groups').append(v.render().el);
       this.addView(v);
@@ -47,7 +72,6 @@
         if (m.get('name') === value && m.cid == cid && !m.get('removed')) {
           // Set active group
           m.set('active', true);
-          console.log(m.attributes.symbol)
           sessionStorage.setItem('currentSymbol', m.attributes.symbol);
           // Change sources_collection global variable :S
           sources_collection = m.getSources();
--- app/assets/javascripts/editor/MapOverlays/GeoCATMarker.js
+++ app/assets/javascripts/editor/MapOverlays/GeoCATMarker.js
@@ -99,7 +99,7 @@
             color = "#066FB6"; //blue
           }
 
-          var shape = this.symbol;
+          var shape = this.symbol || '♥';
           if (sessionStorage.getItem('currentSymbol'))
             shape = sessionStorage.getItem('currentSymbol');
           
