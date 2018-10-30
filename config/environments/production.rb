be035ee79c0728a3ff10b3616789f6a64f401469
Simao Belchior <simaobelchior@gmail.com>
2018-10-30 12:14:15 +0000

    Updates settings adds GMAPS_KEY

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,8 @@ GeocatApp::Application.configure do
   # In production, Apache or nginx will already do this
   config.serve_static_assets = false
 
+  config.assets.compile = false
+
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
