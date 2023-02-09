ac812d01e065c9daff827ff3ef854bfeac70a816
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2023-02-09 14:04:51 +0100

    Fixed tests on ruby 2.7

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,25 +4,60 @@ require 'rails/test_help'
 
 
 # monkey patch for ruby 2.6 and Rails < 5
-# a workaround to avoid MonitorMixin double-initialize error
-# https://github.com/rails/rails/issues/34790#issuecomment-681034561
-if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.6.0')
-  if Gem::Version.new(Rails.version) < Gem::Version.new('5.0.0')
+# From https://github.com/rails/rails/issues/34790
+#
+# This is required because of an incompatibility between Ruby 2.6 and Rails 4.2, which the Rails team is not going to fix.
+
+rb_version = Gem::Version.new(RUBY_VERSION)
+
+if rb_version >= Gem::Version.new('2.6') && Gem::Version.new(Rails.version) < Gem::Version.new('5')
+  if ! defined?(::ActionController::TestResponse)
+    raise "Needed class is not defined yet, try requiring this file later."
+  end
+
+  if rb_version >= Gem::Version.new('2.7')
+    puts "Using #{__FILE__} for Ruby 2.7."
+
     class ActionController::TestResponse < ActionDispatch::TestResponse
       def recycle!
-        if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('2.7.0')
-          @mon_data = nil
-          @mon_data_owner_object_id = nil
-        else
-          @mon_mutex = nil
-          @mon_mutex_owner_object_id = nil
-        end
+        @mon_data = nil
+        @mon_data_owner_object_id = nil
         initialize
       end
     end
+
+    class ActionController::LiveTestResponse < ActionController::Live::Response
+      def recycle!
+        @body = nil
+        @mon_data = nil
+        @mon_data_owner_object_id = nil
+        initialize
+      end
+    end
+
   else
-    $stderr.puts "Monkeypatch for ActionController::TestResponse is no longer needed"
+    puts "Using #{__FILE__} for Ruby 2.6."
+
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        @mon_mutex = nil
+        @mon_mutex_owner_object_id = nil
+        initialize
+      end
+    end
+
+    class ActionController::LiveTestResponse < ActionController::Live::Response
+      def recycle!
+        @body = nil
+        @mon_mutex = nil
+        @mon_mutex_owner_object_id = nil
+        initialize
+      end
+    end
+
   end
+else
+  puts "#{__FILE__} no longer needed."
 end
 # end monkeyy patch
 
